d76616b904ddc7fa8ba0266d388f5d9142855984
me <me@box>
2023-10-03 23:14:21 +0500

    area fix

--- take_sceenshot.sh
+++ take_sceenshot.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [[ $@ == *'-clipboard'* ]]
-then
-	scrot -e 'xclip -selection clipboard -t image/png -i $f && rm $f'
-	notify-send "Screenshot" "Copied in clipboard"
-else
-	scrot ~/Pictures/%b%d_%H_%M_%S.png
-	notify-send "Screenshot" "Screenshot saved!"
+arguments=''
+
+if [[ $@ == *'--area'* ]]; then
+    # Wait for a brief moment to give the user time to select the area manually
+    sleep 1
+    arguments='-s '
 fi
 
+if [[ $@ == *'--clipboard'* ]]; then
+    notify-send "head" "$arguments"
+    # Use the scrot command directly without the -s option for area selection
+    scrot $arguments -e 'xclip -selection clipboard -t image/png -i $f && rm $f'
+    notify-send "Screenshot" "Copied to clipboard"
+else
+    scrot $arguments ~/Pictures/%b%d_%H_%M_%S.png
+    notify-send "Screenshot" "Screenshot saved!"
+fi
 
