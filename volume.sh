3044942d61c4609df2f1e1bd2a58876eb3c93ba6
me <me@box>
2023-10-04 20:24:59 +0500

    remade notifications

--- volume.sh
+++ volume.sh
@@ -1,29 +1,69 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: Incorrect number of arguments. Usage: $0 [inc|dec|mute]"
-    exit 1
-fi
+# Adwaita theme
+# audio_muted="audio-volume-muted-symbolic.symbolic"
+# audio_low="audio-volume-low-symbolic.symbolic"
+# audio_medium="audio-volume-medium-symbolic.symbolic"
+# audio_high="audio-volume-high-symbolic.symbolic"
 
-# Determine the operation based on the argument and print the corresponding number
+# Breeze
+audio_muted="audio-volume-muted"
+audio_low="audio-volume-low"
+audio_medium="audio-volume-medium"
+audio_high="audio-volume-high"
+
+function get_volume {
+	pactl get-sink-volume @DEFAULT_SINK@ | grep '%' | cut -d "/" -f 2 | cut -d '%' -f 1 | sed 's/^[ \t]*//'
+}
+
+function is_muted {
+	pactl get-sink-mute @DEFAULT_SINK@ | cut -d ' ' -f 2
+}
+
+function send_notification {
+	# volume=`get_volume`
+	volume=$(get_volume)
+	muted=$(is_muted)
+
+
+	if [[ "$volume" = "0" ]]; then
+		icon=$audio_muted
+	elif [[ "$volume" -lt "30" ]]; then
+		icon=$audio_low
+	elif [[ "$volume" -lt "65" ]]; then
+		icon=$audio_medium
+	else
+		icon=$audio_high
+	fi
+
+	if [[ "$muted" = "yes" ]]; then
+		notify-send "volume muted" -i "$audio_muted" -a "volume.sh"
+	elif [[ "$volume" = "0" ]]; then
+		notify-send "silient mode" -i "$icon" -a "volume.sh"
+	else
+		bar=$(seq -s "─" $(($volume/3)) | sed 's/[0-9]//g')
+		notify-send "$bar" -i "$icon" -t 2000 -a "volume.sh"
+	fi
+
+}
+
+
 case "$1" in
-    "inc")
+    "up")
         pactl set-sink-volume @DEFAULT_SINK@ +5%
-		notify-send "Volume Increased" "+5%" -i audio-volume-high-symbolic.symbolic
         ;;
-    "dec")
+    "down")
         pactl set-sink-volume @DEFAULT_SINK@ -5%
-		notify-send "Volume Decreased" "\-5%" -i audio-volume-high-symbolic.symbolic
         ;;
     "mute")
         pactl set-sink-mute @DEFAULT_SINK@ toggle
-		notify-send "Volume Muted" "0%" -i audio-volume-high-symbolic.symbolic
         ;;
     *)
-        echo "Error: Invalid argument. Usage: $0 [inc|dec|mute]"
+        echo "Error: Invalid argument. Usage: $0 [up|down|mute]"
         exit 1
         ;;
 esac
 
+send_notification
 exit 0
 
