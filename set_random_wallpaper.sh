99d81476ce40ef18c6601a0e8ca16d1653a166b6
me <me@box>
2023-10-05 23:55:48 +0500

    removed old commented code

--- set_random_wallpaper.sh
+++ set_random_wallpaper.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-IMAGES_DIRECTORY=$1
-
-filename=`/bin/ls $IMAGES_DIRECTORY/*jpg | xargs -n 1 basename | shuf -n 1`
+filename=`/bin/ls $1/*png | xargs -n 1 basename | shuf -n 1`
 notify-send "wallpaper changed" "$filename" -i "image-jpeg" -a "wallpaper-change"
 
-# feh --bg-fill --randomize $IMAGES_DIRECTORY
-feh --bg-fill "$IMAGES_DIRECTORY/$filename"
+feh --bg-fill "$1/$filename"
 
