104c1bcdc4873bab3a7fc38d1bc67fa449e701de
= <me@box>
2023-10-07 21:40:47 +0500

    added oh-my-zsh installation

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,4 +2,8 @@
 # Установка плагинов для nnn
 sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
 
+# Установка oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 
