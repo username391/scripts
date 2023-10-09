54dd6fceff7bceda431abc066026339fc2c65094
= <me@box>
2023-10-10 01:55:46 +0500

    now it can take any file form folder, not only png

---
+++ backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BACKUP_DIR="/run/media/me/BACKUP"
+declare -A CONNECTIONS=(
+    ["$HOME/wallpapers/"]="$BACKUP_DIR/02_pictures/wallpapers/"
+    ["$HOME/accs.kdbx"]="$BACKUP_DIR/__backup/accs.kdbx"
+    ["$HOME/work/"]="$BACKUP_DIR/__backup/work/"
+    ["$HOME/projects/"]="$BACKUP_DIR/__backup/projects/"
+)
+
+IGNORED_ITEMS=(
+	".git"
+	"__pycache__"
+	"work"
+	"projects"
+)
+EXCLUDE=""
+
+# Перебираем игнорируемые элементы и добавляем их в строку параметров --exclude
+for item in "${IGNORED_ITEMS[@]}"; do
+    EXCLUDE+=" --exclude=$item"
+done
+
+from_backup=false
+
+while getopts ":f" opt; do
+    case $opt in
+        from-backup) from_backup=true ;;
+        \?) echo "Неверный аргумент: -$OPTARG" >&2; exit 1 ;;
+    esac
+done
+
+for key in "${!CONNECTIONS[@]}"; do
+    value=${CONNECTIONS[$key]}
+	echo "$key -> $value"
+    if [ "$from_backup" = true ]; then
+        rsync -avzh "$value" "$key"
+    else
+        # rsync -avzh --relative --rsync-path="cd \"$BACKUP_DIR\" && rsync" "$key" "$value"
+		rsync -avzh $EXCLUDE $key $value
+    fi
+done
+
+echo "Копирование завершено!"
+
--- set_random_wallpaper.sh
+++ set_random_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filename=`/bin/ls $1/*png | xargs -n 1 basename | shuf -n 1`
+filename=`/bin/ls $1/ | xargs -n 1 basename | shuf -n 1`
 notify-send "wallpaper changed" "$filename" -i "image-jpeg" -a "wallpaper-change"
 
 feh --bg-fill "$1/$filename"
