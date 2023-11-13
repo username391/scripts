a1cf7d839bc7404ae31799891d76cb500d3107e6
= <me@box>
2023-11-13 19:16:49 +0500

    remade video download script

--- download-video.sh
+++ download-video.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-video_url=$1
-directory=$2
+if [ "$#" -eq 0 ]; then
+    video_url=$(echo "" | dmenu -p "Video url: ")
+    directory=$(ls "$HOME/Videos/" | dmenu -l 5 -p "Путь до папки: ")
 
-yt-dlp -o "$HOME/Videos/$directory/%(title)s [%(id)s].%(ext)s" "$1"
+elif [ "$#" -eq 1 ]; then
+    video_url="$1"
+    directory=$(ls "$HOME/Videos/" | dmenu -l 5 -p "Путь до папки: ")
+else
+	video_url=$1
+	directory=$2
+fi
 
-notify-send "Video downloaded to $directory dir"
+if [ -z "$video_url" ] || [ -z "$directory" ]; then
+	echo "Video url or directory is not passed!"
+	notify-send "Video url or directory is not passed!"
+else
+	$HOME/scripts/yt-dlp.sh -o "$HOME/Videos/$directory/%(title)s [%(id)s].%(ext)s" "$1"
+	notify-send "Video downloaded to $directory dir"
+fi
+
