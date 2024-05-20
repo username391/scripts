8c69648cd2b81676ac95e79dd853d51054dd67b3
me <me@box>
2024-05-20 11:50:37 +0500

    not sure what was that

--- redshift.sh
+++ redshift.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-envFile=$HOME/scripts/redshift-env.sh
+envFile=$HOME/.local/share/redshift-env.sh
 changeValue=300
+
+
+# if [ ! -f "$envFile" ]; then
+#     # Создание файла с указанным содержанием
+#     echo 'export REDSHIFT=off' > "$envFile"
+#     echo 'export REDSHIFT_TEMP=1100' >> "$envFile"
+# 	echo 'created file'
+# fi
+#
+# envFile=$HOME/.local/share/redshift-env.sh
+echo "$REDSHIFT_TEMP"
+
+
 
 changeMode() {
   sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
@@ -14,6 +27,8 @@ changeTemp() {
   then
     sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile 
     redshift -P -O $((REDSHIFT_TEMP+changeValue))
+else
+	echo "error with if"
   fi
 }
 
@@ -25,11 +40,16 @@ case $1 in
       redshift -x
     else
       changeMode "$REDSHIFT" on
-      redshift -O "$REDSHIFT_TEMP"
+	  changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP))
+      # redshift -O "$REDSHIFT_TEMP"
     fi
     ;;
   increase)
+	# echo "inc - $((REDSHIFT_TEMP+changeValue))"
+	echo $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
     changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
+	echo "inc - $((REDSHIFT_TEMP+changeValue))" >> inc.txt
+	echo "inc - $((REDSHIFT_TEMP+changeValue))"
     ;;
   decrease)
     changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
@@ -37,6 +57,7 @@ case $1 in
   temperature)
     case $REDSHIFT in
       on)
+		echo "==$REDSHIFT_TEMP"
         printf "%dK" "$REDSHIFT_TEMP"
         ;;
       off)
