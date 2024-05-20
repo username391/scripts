0f641f2c43cae5b6692bad393627ab41baa82b6e
me <me@box>
2024-05-20 11:51:16 +0500

    ?

--- set_random_wallpaper.sh
+++ set_random_wallpaper.sh
@@ -3,5 +3,5 @@
 filename=`/bin/ls $1/ | xargs -n 1 basename | shuf -n 1`
 notify-send "wallpaper changed" "$filename" -i "image-jpeg" -a "wallpaper-change"
 
-feh --bg-scale "$1/$filename"
+feh --bg-max "$1/$filename"
 
